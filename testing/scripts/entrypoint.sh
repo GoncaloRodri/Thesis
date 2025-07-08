14490f1533a3fad747a904a05e7bbb315c33052a
GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>
2025-07-08 16:03:26 +0100

    :hammer: WIP
    Signed-off-by: GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>

--- testing/scripts/entrypoint.sh
+++ testing/scripts/entrypoint.sh
@@ -11,8 +11,9 @@ mkdir -p /app/conf
 
 cp /app/conf/nodes/"$RELAY_TYPE"/torrc /app/tor/torrc
 
-if [ "$RELAY_TYPE" != "client" ]; then
-    cp -r /app/conf/nodes/"$RELAY_TYPE"/crypto/* /app/tor/
+if [ "$RELAY_TYPE" != "authority" ]; then
+    AUTHORITY_IP=$(getent hosts authority | awk '{ print $1 }')
+    sed -i "s/DirAuthority .*/DirAuthority authority orport=9001 v3ident=251A6199439061376DBDEB65848324E2D5EC89C7 ${AUTHORITY_IP}:9030 A52CA5B56C64D864F6AE43E56F29ACBD5706DDA1/" /app/conf/tor.common.torrc
 fi
 
 cd /app/tor || exit 1
@@ -20,6 +21,6 @@ cd /app/tor || exit 1
 sed -i "s/^Address .*/Address $HOST/" /app/conf/tor.common.torrc
 sed -i "s/^Nickname .*/Nickname $NICKNAME/" /app/conf/tor.common.torrc
 
-cat /app/conf/tor.common.torrc 
+cat /app/conf/tor.common.torrc
 
 (tor -f /app/tor/torrc) | tee /app/logs/tor/"$NICKNAME".tor.log
