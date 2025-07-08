16d49dd5fffdd12b544310081d40d9d7088a7746
GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>
2025-07-08 14:43:29 +0100

    :hammer: WIP
    Signed-off-by: GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>

--- testing/scripts/entrypoint.sh
+++ testing/scripts/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+HOST=$(hostname)
+NICKNAME=$(echo $HOST | tr -d '-')
+
+echo "Starting DPTOR node with nickname: $NICKNAME"
+echo "Hostname: $HOST"
 
 mkdir -p /app/logs/tor
-mkdir -p /app/logs/wireshark/"$RELAY_TYPE"
+mkdir -p /app/logs/wireshark/$NICKNAME
 mkdir -p /app/conf
 
 cp /app/conf/nodes/"$RELAY_TYPE"/torrc /app/tor/torrc
@@ -10,16 +15,11 @@ if [ "$RELAY_TYPE" != "client" ]; then
     cp -r /app/conf/nodes/"$RELAY_TYPE"/crypto/* /app/tor/
 fi
 
-if [ "$RELAY_TYPE" = "hidden_service" ]; then
-    mkdir -p /root/.tor/hidden_service
-    cp -r /app/hidden_service/* /root/.tor/hidden_service/
-fi
-
 cd /app/tor || exit 1
 
-sed -i "s/^Address .*/Address $(hostname -i)/" /app/conf/tor.common.torrc
-sed -i "s/^Nickname .*/Nickname $NAME/" /app/conf/tor.common.torrc
+sed -i "s/^Address .*/Address $HOST/" /app/conf/tor.common.torrc
+sed -i "s/^Nickname .*/Nickname $NICKNAME/" /app/conf/tor.common.torrc
 
 cat /app/conf/tor.common.torrc 
 
-(tor -f /app/tor/torrc) | tee /app/logs/tor/"$NAME".tor.log
+(tor -f /app/tor/torrc) | tee /app/logs/tor/"$NICKNAME".tor.log
