8af8fcaf208f3a80d47ed3bcca821f3dbb429fe6
GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>
2025-07-16 21:41:14 +0100

    :hammer: WIP
    Signed-off-by: GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>

--- testing/scripts/entrypoint.sh
+++ testing/scripts/entrypoint.sh
@@ -6,22 +6,34 @@ NICKNAME=$(echo $HOST | tr -d '-')
 if [ "$RELAY_TYPE" == "authority" ]; then
     sed -i "s/DirAuthority .*/DirAuthority authority orport=9001 v3ident=251A6199439061376DBDEB65848324E2D5EC89C7 ${IP_ADDRESS}:9030 A52CA5B56C64D864F6AE43E56F29ACBD5706DDA1/" /app/conf/tor.common.torrc
 else
-    sleep 10
+    echo "================================================================================"
+    sleep 30
+    echo "Waiting for authority to be ready..."
 fi
 
 mkdir -p /app/logs/tor
 mkdir -p /app/logs/wireshark/$NICKNAME
 mkdir -p /app/conf
 
-cp /app/conf/nodes/"$RELAY_TYPE"/torrc /app/tor/torrc
+if [ ! -d /app/conf/nodes/"$NICKNAME" ]; then
+    mkdir -p /app/conf/nodes/"$NICKNAME"
+    cp /app/conf/nodes/"${RELAY_TYPE}"/torrc /app/conf/nodes/"$NICKNAME"/
+fi
 
-sed -i "s/^Address .*/Address $IP_ADDRESS/" /app/conf/tor.common.torrc
-sed -i "s/^Nickname .*/Nickname $NICKNAME/" /app/conf/tor.common.torrc
+sed -i "s/^Address .*/Address $IP_ADDRESS/" /app/conf/nodes/"$NICKNAME"/torrc
+sed -i "s/^Nickname .*/Nickname $NICKNAME/" /app/conf/nodes/"$NICKNAME"/torrc
+wait 
+
+cp /app/conf/nodes/"$NICKNAME"/torrc /app/tor/torrc
 
 if [ "$RELAY_TYPE" != "client" ]; then
-    cp -r /app/conf/nodes/$RELAY_TYPE/crypto/* /app/tor/
+    cp -r /app/conf/nodes/"$NICKNAME"/crypto/* /app/tor/
 fi
 
 cd /app/tor || exit 1
+echo "================================================================================"
+echo "NICKNAME: $NICKNAME"
+echo "IP_ADDRESS: $IP_ADDRESS"
+cat /app/tor/torrc
 
 (tor -f /app/tor/torrc) | tee /app/logs/tor/"$NICKNAME".tor.log
--- testing/scripts/tester/swarm.sh
+++ testing/scripts/tester/swarm.sh
@@ -9,12 +9,11 @@ ABS_DIR="$(cd "${SCRIPT_DIR}/../../../" && pwd)"
 source "${SCRIPT_DIR}/src/utils.sh"
 
 BOOTSTRAP_SLEEP=1
-MAX_TIME_TO_BOOTSTRAP=90
+MAX_TIME_TO_BOOTSTRAP=300
 PERFORMANCE_BOOTSTRAP_COUNTER=5
 COMPOSE_FILE="${ABS_DIR}/swarm.docker-compose.yml"
 
-AUTHORITY_IP="10.5.0.1"
-AUTHORITY_SSH="root@${AUTHORITY_IP}"
+AUTHORITY_IP="127.0.0.1"
 
 TOKEN=""
 
@@ -23,32 +22,58 @@ VERBOSE=true
 RELAYS=(
 )
 
-CLIENT="ubuntu@aaa"
+clean() {
+    log_info "Cleaning up Swarm environment..."
+    docker stack rm thesis || true
+    docker swarm leave --force || true
+    docker network rm swarm || true
+    docker service rm $(docker service ls -q) || true
+    echo
+}
 
 connect() {
+    log_info "Building Docker images..."
     docker buildx build -t gugarodri/dptor_node:latest -f "${ABS_DIR}"/testing/docker/node.Dockerfile "${ABS_DIR}"/
-
+    docker buildx build -t gugarodri/dptor_httpserver:latest -f "${ABS_DIR}"/testing/docker/httpserver.Dockerfile "${ABS_DIR}"/
+    
+    log_info "Pushing Docker images to registry..."
     docker push gugarodri/dptor_node:latest
+    docker push gugarodri/dptor_httpserver:latest
 
-    docker swarm leave --force || true
+    
 
-    TOKEN=$(docker swarm init --advertise-addr "${AUTHORITY_IP}")
+    log_info "Initializing Docker Swarm..."
+    SWARM_JOIN_CMD=$(docker swarm init --advertise-addr "${AUTHORITY_IP}" | grep "join --token")
 
-    for relay in "${RELAYS[@]}"; do
-        ssh "${relay}" "docker swarm join --token ${TOKEN} ${AUTHORITY_IP}:2377"
-    done
+    log_info "Swarm initialized with token: ${TOKEN}"
 
-    ssh "${CLIENT}" "docker swarm join --token ${TOKEN} ${AUTHORITY_IP}:2377"
+    log_info "Creating Docker network..."
+    docker network create --driver overlay --attachable swarm
+
+    # for relay in "${RELAYS[@]}"; do
+    #     ssh "${relay}" "${SWARM_JOIN_CMD}"
+    # done
+
+    #ssh "${CLIENT}" "${SWARM_JOIN_CMD}"
 
 }
 
-run() {
-    CLIENT_DOCKER_SERVICE=$(docker services ls | grep client | awk '{print $10}')
+check_boot() {
+    logs_path="${ABS_DIR}/testing/logs/tor/*"
+    # shellcheck disable=SC2086
+    BSED=$(grep -l -R "Bootstrapped 100%" $logs_path | wc -l)
+    echo "$BSED"
+}
 
+get_server_ip() {
+    "$(docker exec "$(docker container ls | grep "server" | awk '{print $1}')" hostname -i )"
+}
+
+get_client_id() {
+    "$(docker container ls | grep "client" | awk '{print $1}')"
 }
 
 deploy() {
-    docker stack rm thesis
     while true; do
         log_info "Deploying Services to Swarm..."
         docker stack deploy --detach=false -c "${COMPOSE_FILE}" thesis
@@ -59,7 +84,7 @@ deploy() {
 
         while [ $ELAPSED -lt $MAX_TIME_TO_BOOTSTRAP ]; do
             sleep "$BOOTSTRAP_SLEEP"
-            a=$(check_bootstrapped)
+            a=$(check_boot)
             if [ "$a" -eq $PERFORMANCE_BOOTSTRAP_COUNTER ]; then
                 break 2
             fi
@@ -75,35 +100,21 @@ deploy() {
         sleep 3
     done
 
-    run
-
 }
 
-log_info "Starting Swarm Experiment..."
+request() {
+   curl --socks5 127.0.0.1:9000 -H 'Cache-Control: no-cache' -w "Code: %{response_code}\nTime to first byte: %{time_starttransfer}s\nTotal time: %{time_total}s\nDownload speed: %{speed_download} bytes/sec\n" -o /dev/null "$(get_server_ip)":5000/bytes/"$2"
+}
+run() {
+    log_info "Running tests against the Swarm deployment..."
 
-while true; do
-    log_info "Deploying Services to Swarm..."
-    docker stack deploy --detach=false -c "${COMPOSE_FILE}" thesis
+    request "51200"
+}
 
-    START=$(date +%s)
-    END=$(date +%s)
-    ELAPSED=$((END - START))
 
-    while [ $ELAPSED -lt $MAX_TIME_TO_BOOTSTRAP ]; do
-        sleep "$BOOTSTRAP_SLEEP"
-        a=$(check_bootstrapped)
-        if [ "$a" -eq $PERFORMANCE_BOOTSTRAP_COUNTER ]; then
-            break 2
-        fi
-        END=$(date +%s)
-        ELAPSED=$((END - START))
-        if [[ "$VERBOSE" == true ]]; then
-            echo -ne "⚠️ \e[33mWarning: Tor Network is not bootstrapped yet! ($a of $PERFORMANCE_BOOTSTRAP_COUNTER) [$ELAPSED s]\e[0m"\\r
-        fi
-    done
+clean
+connect
+deploy
+run
 
-    log_error "Swarm Deployment Failed" "Tor Network failed to bootstrap within $MAX_TIME_TO_BOOTSTRAP seconds. Retrying..."
-    docker stack rm thesis
-done
-
-log_info "Swarm Deployment Successful!"
+log_sucess "Swarm deployment and tests completed successfully!"
