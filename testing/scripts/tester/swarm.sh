e64ae3acf7bed8abdcbc375003489ec34719b1a5
GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>
2025-08-01 14:11:22 +0100

    :hammer: WIP
    Signed-off-by: GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>

--- testing/scripts/tester/swarm.sh
+++ testing/scripts/tester/swarm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+declare -a nodes=("authority" "relay1" "relay2" "exit1" "client")
+declare -a machines
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ABS_DIR="$(cd "${SCRIPT_DIR}/../../../" && pwd)"
 
@@ -11,8 +14,20 @@ MANAGER_IP=127.0.0.1
 
 docker swarm leave --force
 
-docker swarm init --advertise-addr $MANAGER_IP
+JOIN_CMD=$(docker swarm init --advertise-addr $MANAGER_IP | grep "join --token")
 
 # TODO: JOIN WORKER NODES
+for node in "${nodes[@]}"; do
+  ssh ${node} "${JOIN_CMD}"
+done
 
+docker node ls
+
+# TODO: NAME NODES TO FUNCTIONALITY
+
+docker node update --label-add node=authority authority
+docker node update --label-add node=relay1 relay1
+docker node update --label-add node=relay2 relay2
+docker node update --label-add node=exit1 exit1
+docker node update --label-add node=client client
 
